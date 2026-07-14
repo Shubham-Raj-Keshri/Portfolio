0d66c0c37353223fe6b9f1e9836a62222d7b1369
Shubham Raj Keshri <keshrishubham883@gmail.com>
2026-07-14 15:16:32 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/Portfolio",
+  assetPrefix: "/Portfolio/",
+  trailingSlash: true,
+
   images: {
     unoptimized: true,
   },
 };
+
+export default nextConfig;
