d806dd4292076436520276f44748dbae63d64d85
Shubham Raj Keshri <keshrishubham883@gmail.com>
2026-07-14 20:20:59 +0530

    Update resume

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/Portfolio",
-  assetPrefix: "/Portfolio/",
+
+  basePath: isProd ? "/Portfolio" : "",
+  assetPrefix: isProd ? "/Portfolio/" : "",
+
   trailingSlash: true,
 
   images: {
